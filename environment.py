2d6dbd637063fa5e9165fb0b91bad5f496d8a373
Yacine Benihaddadene <yacine.benihaddadene@epita.fr>
2025-11-12 17:07:59 +0100

    feat(environment): added configurable flags for explicit lives penalty on reward and frame max pooling

--- environment.py
+++ environment.py
@@ -5,15 +5,28 @@ import torch
 from PIL import Image
 
 class BreakoutWrapper(gym.Wrapper):
-    def __init__(self,game="ALE/Breakout-v5", render_mode="human", frame_skip: int = 4,frame_stack:int=4, device="cpu"):
-        env = gym.make(id=game, render_mode=render_mode) if render_mode else gym.make(id=game)
+    def __init__(self,game: str = "ALE/Breakout-v5", 
+                 render_mode: str | None = "human", 
+                 frame_skip: int = 4,
+                 frame_stack:int= 4,
+                 device: str = "cpu", 
+                 lives_penalty: bool = False,
+                 max_frame: bool = False):
+        
+        env = gym.make(id=game, render_mode=render_mode, frameskip=1, repeat_action_probability=0.0)
         super().__init__(env)
         self.frame_skip = frame_skip
         self.frame_stack_len = frame_stack
         self.frame_buffer = deque(maxlen=frame_skip)
         self.frame_stack = deque(maxlen=frame_stack)
+        # flag to perform or not a penalty on reward when losing a life
+        self.lives_penalty = lives_penalty
+        # flag to perform or not pointwise max of the last two frames in the skipping process: From paper Nature on DQN (2015)
+        self.max_frame = max_frame 
+
         for _ in range(frame_stack):
             self.frame_stack.append(torch.zeros((1,1,84,84), dtype=torch.float32).to(device=device))
+
         self.device = device
         self.lives = env.unwrapped.ale.lives()
 
@@ -24,22 +37,29 @@ class BreakoutWrapper(gym.Wrapper):
             obs, reward, done, truncated, info = self.env.step(action=action)
             total_reward+=reward
 
-            current_lives = info["lives"]
-            if current_lives < self.lives:
-                total_reward -=1
-                self.lives = current_lives
+            if self.lives_penalty:
+                current_lives = info["lives"]
+                if current_lives < self.lives:
+                    total_reward -=1
+                    self.lives = current_lives
 
             self.frame_buffer.append(obs)
 
             if done or truncated:
                 break
         
-        # the pointwise maximum of the two last frames in the skip phase make one observation => 
-        # we will stack 4 of those observations to make a state for the dqn
-        max_frame = np.max(list(self.frame_buffer)[-2:], axis=0) 
-        max_frame = self._preprocess(max_frame)
 
-        self.frame_stack.append(max_frame)
+        if self.max_frame:
+            # the pointwise maximum of the two last frames in the skip phase make one observation => 
+            # we will stack 4 of those observations to make an input frame for the dqn
+            observation = np.max(list(self.frame_buffer)[-2:], axis=0) 
+        else:
+            observation = self.frame_buffer[-1]
+
+
+        observation = self._preprocess(observation)
+
+        self.frame_stack.append(observation)
         observation = torch.cat(list(self.frame_stack), dim=1) # concatenate along the channel dimension
 
         total_reward = torch.tensor(total_reward).view(1,-1).float().to(device=self.device)
@@ -71,10 +91,11 @@ class BreakoutWrapper(gym.Wrapper):
     def reset(self, **kwargs):
         self.frame_buffer = deque(maxlen=self.frame_skip)
         self.frame_stack = deque(maxlen=self.frame_stack_len)
+
         for _ in range(self.frame_stack_len):
             self.frame_stack.append(torch.zeros((1,1,84,84), dtype=torch.float32).to(device=self.device))
-        obs, info = self.env.reset(**kwargs)
 
+        obs, info = self.env.reset(**kwargs)
         self.lives = self.env.unwrapped.ale.lives()
 
         obs = self._preprocess(obs)
